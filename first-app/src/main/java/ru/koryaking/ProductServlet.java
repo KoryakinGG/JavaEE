6833381e847c0f61244762af8c4dd8ac907f4047
KoryakinGG <koryaking@mail.ru>
2021-02-17 15:59:14 +0300

    добавил удаление

--- first-app/src/main/java/ru/koryaking/ProductServlet.java
+++ first-app/src/main/java/ru/koryaking/ProductServlet.java
@@ -48,29 +48,42 @@ public class ProductServlet extends HttpServlet {
             getServletContext().getRequestDispatcher("/WEB-INF/product_form.jsp").forward(req, resp);
         } else if (req.getPathInfo().equals("/delete")) {
             // TODO delete product
-
+            long id;
+            try {
+                id = Long.parseLong(req.getParameter("id"));
+            } catch (NumberFormatException ex) {
+                resp.setStatus(400);
+                return;
+            }
+            productRepository.deleteById(id);
+            resp.sendRedirect(getServletContext().getContextPath()+ "/product");
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        long id;
-        try {
-            id = Long.parseLong(req.getParameter("id"));
-        } catch (NumberFormatException ex) {
-            resp.setStatus(400);
-            return;
+        if(req.getPathInfo().equals("/edit")) {
+            long id;
+            try {
+                id = Long.parseLong(req.getParameter("id"));
+            } catch (NumberFormatException ex) {
+                resp.setStatus(400);
+                return;
+            }
+            BigDecimal price;
+            try {
+                price = new BigDecimal(req.getParameter("price"));
+            } catch (NumberFormatException ex) {
+                resp.setStatus(400);
+                return;
+            }
+            Product product = new Product(id, req.getParameter("name"), req.getParameter("description"), price);
+            productRepository.saveOrUpdate(product);
+            resp.sendRedirect(getServletContext().getContextPath() + "/product");
         }
-        BigDecimal price;
-        try {
-            price = new BigDecimal(req.getParameter("price"));
-        } catch (NumberFormatException ex) {
-            resp.setStatus(400);
-            return;
-        }
+        else if (req.getPathInfo().equals("/product_add_form")) {
 
-       Product product = new Product(id, req.getParameter("name"), req.getParameter("description"),price);
-        productRepository.saveOrUpdate(product);
-        resp.sendRedirect(getServletContext().getContextPath()+ "/product");
+            getServletContext().getRequestDispatcher("/WEB-INF/product_add_form.jsp").forward(req, resp);
+        }
     }
 }
